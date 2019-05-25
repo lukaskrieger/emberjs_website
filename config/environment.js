6903e0e4276acbf67ba5ec8308893347c6b780e5
Lukas Krieger <lukas@macbook.achiever>
2019-05-25 12:48:26 +0200

    Some styling of the about page

--- config/environment.js
+++ config/environment.js
@@ -20,6 +20,18 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    // Add options here
+    fontawesome: {
+      icons: {
+        'free-brands-svg-icons': [
+          'linkedin-in',
+          'github',
+          'researchgate',
+          'ember'
+        ]
+      }
     }
   };
 
